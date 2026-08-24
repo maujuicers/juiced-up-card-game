7d095c8d094f629e4a3663258ddf4a0e40e58096
TheFefel <flxkhn0602@gmail.com>
2026-08-24 19:01:33 +0200

    feature: connected buttons to script without content

--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -1,1 +1,20 @@
 extends Node3D
+
+
+func _on_start_game_button_pressed() -> void:
+	#TODO: get_tree().change_to_packed()
+	pass
+
+
+func _on_tutorial_button_pressed() -> void:
+	#TODO: Open tutorial popup
+	pass
+
+
+func _on_settings_button_pressed() -> void:
+	#TODO: Open settings menu
+	pass
+
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
