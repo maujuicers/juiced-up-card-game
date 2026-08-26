354f5fb117ea80a8b79f4c45377bc2872a02c34f
Lorenzo <dev@ozoromo.com>
2026-08-26 21:19:02 +0200

    Remove player movement & re-work Juice (#31)
    * Rework of the juice meter.
    - Created new juice meter scene with script
    - Moved Juice relevant functionality from player and UI element to scene
    
    * More related changes
    
    * Switched out scenes and placed player camera
    
    * Added id-token: write and contents: read for FlakeHub

---
+++ scripts/player/juice.gd
@@ -0,0 +1,32 @@
+extends Node
+
+class_name Juice
+
+@export var max_juice: int = 100
+@export var current_juice: int = 100
+var drain_enabled: bool = true
+
+@onready var timer: Timer = $Timer
+
+signal juice_changed(current: int)
+signal juice_empty
+
+func _ready() -> void:
+	timer.timeout.connect(_on_timeout)
+	timer.start()
+
+func set_juice(amount: int) -> void:
+	current_juice = clamp(amount, 0, max_juice)
+	juice_changed.emit(current_juice)
+	if current_juice < 1:
+		timer.stop()
+		juice_empty.emit()
+
+func reset() -> void:
+	set_juice(max_juice)
+	timer.start()
+
+func _on_timeout() -> void:
+	if not drain_enabled:
+		return
+	set_juice(current_juice - 1)
--- scripts/ui/juice_meter.gd
+++ scripts/ui/juice_meter.gd
@@ -2,22 +2,12 @@ extends ProgressBar
 
 class_name JuiceMeter
 
-@export var player: PlayerController
+@export var juice: Juice
 
-@onready var timer: Timer = $Timer
+func _ready() -> void:
+	juice.juice_changed.connect(update_value)
+	value = juice.current_juice
+	max_value = juice.max_juice
 
-var stop_juice_drop: bool = false
-
-func _init_juice_meter() -> void:
-	player.juice_changed.connect(update_juice)
-	timer.timeout.connect(_on_timeout)
-	update_juice()
-	timer.start()
-
-func update_juice():
-	value = player.current_juice
-
-func _on_timeout() -> void:
-	player.current_juice -= 1
-	update_juice()
-	timer.start()
+func update_value(current: int) -> void:
+	value = current
