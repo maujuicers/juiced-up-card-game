2a4583f6a34512e6b224bbaef934a9dcb38018d1
TheFefel <flxkhn0602@gmail.com>
2026-08-26 22:01:04 +0200

    fix: audio not longer too loud with settings

--- scripts/ui/settings_menu.gd
+++ scripts/ui/settings_menu.gd
@@ -18,15 +18,15 @@ func _on_save_button_pressed() -> void:
 	pass # Replace with function body.
 
 func _on_master_volume_slider_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_linear(0, value)
+	AudioServer.set_bus_volume_linear(0, value/100)
 	master_volume_value_label.text = str(int(value))
 
 
 func _on_music_slider_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_linear(3, value)
+	AudioServer.set_bus_volume_linear(3, value/100)
 	music_value_label.text = str(int(value))
 
 func _on_sfx_slider_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_linear(1, value)
-	AudioServer.set_bus_volume_linear(2, value)
+	AudioServer.set_bus_volume_linear(1, value/100)
+	AudioServer.set_bus_volume_linear(2, value/100)
 	sfx_value_label.text = str(int(value))
