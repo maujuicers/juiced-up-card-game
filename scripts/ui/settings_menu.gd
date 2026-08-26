4915432930190bd17686d4f80240444be74efb55
TheFefel <flxkhn0602@gmail.com>
2026-08-26 20:06:37 +0200

    feature: connected functionality to settings and removed resolution settings

--- scripts/ui/settings_menu.gd
+++ scripts/ui/settings_menu.gd
@@ -1,10 +1,32 @@
 extends Control
 
-
-func _on_apply_button_pressed() -> void:
-	#TODO: Add config file save for settings
-	pass
-
+@export var master_volume_value_label: Label
+@export var music_value_label: Label
+@export var sfx_value_label: Label
 
 func _on_close_button_pressed() -> void:
 	self.hide()
+
+
+func _on_fullscreen_button_toggled(toggled_on: bool) -> void:
+	if toggled_on == true:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+	else:
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+
+func _on_save_button_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_master_volume_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_linear(0, value)
+	master_volume_value_label.text = str(int(value))
+
+
+func _on_music_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_linear(3, value)
+	music_value_label.text = str(int(value))
+
+func _on_sfx_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_linear(1, value)
+	AudioServer.set_bus_volume_linear(2, value)
+	sfx_value_label.text = str(int(value))
